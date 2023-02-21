18e59f1ce179af854556b8fa71bbde772233a10a
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-21 12:23:26 +0530

    Added Program to use JSON Objects and Print the objects

--- Program.cs
+++ Program.cs
@@ -5,6 +5,39 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Inventory Data Management");
+            string path = "D:\\c#\\257\\InventoryManagement\\InventoryManagement\\InventoryJson.json";
+
+            FetchInventory fetchInventory = new FetchInventory();
+
+            InventoryDetails data = fetchInventory.Read(path);
+
+            Console.WriteLine("\nTypes of Rice: ");
+
+            for (int i = 0; i < data.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typesOfRice[i].name);
+                Console.WriteLine("weight: "+data.typesOfRice[i].weight +"kg");
+                Console.WriteLine("price: "+data.typesOfRice[i].price +"/kg");
+                Console.WriteLine("-------------------------------");
+            }
+
+            Console.WriteLine("\nTypes of Pulses: ");
+            for (int i = 0; i < data.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(data.typesOfPulses[i].name);
+                Console.WriteLine("weight: " + data.typesOfPulses[i].weight+"kg");
+                Console.WriteLine("price: " + data.typesOfPulses[i].price+"/kg");
+                Console.WriteLine("-------------------------------");
+            }
+
+            Console.WriteLine("\nTypes of Wheat: ");
+            for (int i = 0; i < data.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(data.typesOfWheat[i].name);
+                Console.WriteLine("weight: " + data.typesOfWheat[i].weight+"kg");
+                Console.WriteLine("price: " + data.typesOfWheat[i].price+"/kg");
+                Console.WriteLine("-------------------------------");
+            }
         }
     }
 }
